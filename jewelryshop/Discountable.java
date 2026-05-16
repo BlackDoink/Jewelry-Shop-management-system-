86e4910bd7205c24956400ec43ebbea83f952e25
BlackDoink <turjo482@gmail.com>
2026-05-16 06:12:30 +0600

    Delete jewelryshop directory

--- jewelryshop/Discountable.java
+++
@@ -1,8 +0,0 @@
-package com.jewelryshop;
-
-public interface Discountable {
-    double applyDiscount(double percentage); // existing method
-
-    double getDiscount();
-    void setDiscount(double percentage);
-}
